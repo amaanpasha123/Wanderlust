1a9d6b8cfd71bb906a11325c8fa14575755db73a
Amaan Pasha <Amaanpasha23@gmail.com>
2025-01-21 16:00:26 +0530

    signup page is been made in this

--- routes/dist/user.dev.js
+++ routes/dist/user.dev.js
@@ -3,7 +3,26 @@
 var express = require("express");
 
 var router = express.Router();
+
+var User = require("..models/user.js");
+
 router.get("/signup", function (req, res) {
-  res.render("user/signup");
+  res.render("users/signup");
+});
+router.post("/sigup", function _callee(req, res) {
+  var _req$body, username, id, password;
+
+  return regeneratorRuntime.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _req$body = req.body, username = _req$body.username, id = _req$body.id, password = _req$body.password;
+
+        case 1:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
 });
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const router = express.Router();
-
+const User = require("..models/user.js");
 
 router.get("/signup", (req, res) => {
-    res.render("user/signup");
+    res.render("users/signup");
 });  
 
+router.post("/sigup", async(req, res)=>{
+    let{username, id, password} = req.body;
 
+});
 
 
 
 
 module.exports = router;
+
