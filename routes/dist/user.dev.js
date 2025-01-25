1d65fab4251f66fd54a62a28a7fe9db4c72a48ca
Amaan Pasha <Amaanpasha23@gmail.com>
2025-01-25 13:13:52 +0530

    all are good

--- routes/dist/user.dev.js
+++ routes/dist/user.dev.js
@@ -18,25 +18,34 @@ router.post("/signup", wrapAsyc(function _callee(req, res) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
+          _context.prev = 0;
           _req$body = req.body, username = _req$body.username, email = _req$body.email, password = _req$body.password;
           newuser = new User({
             email: email,
             username: username
           });
-          _context.next = 4;
+          _context.next = 5;
           return regeneratorRuntime.awrap(User.register(newuser, password));
 
-        case 4:
+        case 5:
           registerUser = _context.sent;
           console.log(registerUser);
           req.flash("success", "user was registered");
           res.redirect("/listings");
+          _context.next = 15;
+          break;
 
-        case 8:
+        case 11:
+          _context.prev = 11;
+          _context.t0 = _context["catch"](0);
+          req.flash("error", _context.t0.message);
+          res.redirect("/signup");
+
+        case 15:
         case "end":
           return _context.stop();
       }
     }
-  });
+  }, null, null, [[0, 11]]);
 }));
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -12,12 +12,19 @@ router.get("/signup", (req, res) => {
 
 
 router.post("/signup",wrapAsyc( async(req, res)=>{
-    let{username, email, password} = req.body;
+    try{
+        let{username, email, password} = req.body;
     const newuser = new User({email, username});
     const registerUser = await User.register(newuser, password);
     console.log(registerUser);
     req.flash("success","user was registered");
     res.redirect("/listings");
+    }
+    catch(e){
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+    
 }));
 
 
