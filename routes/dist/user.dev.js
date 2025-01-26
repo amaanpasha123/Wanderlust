812ffe91aeec3540fa45a7468dea544aa7a28102
Amaan Pasha <Amaanpasha23@gmail.com>
2025-01-26 23:27:44 +0530

    new one is added in it

--- routes/dist/user.dev.js
+++ routes/dist/user.dev.js
@@ -4,56 +4,49 @@ var express = require("express");
 
 var router = express.Router();
 
-var User = require("../models/user");
+var User = require("../models/user.js");
 
-var wrapAsyc = require("../utils/wrapAsyc");
+var wrapAsync = require("../utils/wrapAsyc.js");
 
-var passport = require("passport");
+var passport = require("passport"); // Signup route
+
 
 router.get("/signup", function (req, res) {
   res.render("users/signup");
 });
-router.post("/signup", wrapAsyc(function _callee(req, res) {
-  var _req$body, username, email, password, newuser, registerUser;
+router.post("/signup", wrapAsync(function _callee(req, res) {
+  var _req$body, username, email, password, newUser, registeredUser;
 
   return regeneratorRuntime.async(function _callee$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
-          _context.prev = 0;
           _req$body = req.body, username = _req$body.username, email = _req$body.email, password = _req$body.password;
-          newuser = new User({
+          newUser = new User({
             email: email,
             username: username
           });
-          _context.next = 5;
-          return regeneratorRuntime.awrap(User.register(newuser, password));
+          _context.next = 4;
+          return regeneratorRuntime.awrap(User.register(newUser, password));
 
-        case 5:
-          registerUser = _context.sent;
-          console.log(registerUser);
-          req.flash("success", "user was registered");
+        case 4:
+          registeredUser = _context.sent;
+          console.log(registeredUser);
+          req.flash("success", "User was registered successfully!");
           res.redirect("/listings");
-          _context.next = 15;
-          break;
 
-        case 11:
-          _context.prev = 11;
-          _context.t0 = _context["catch"](0);
-          req.flash("error", _context.t0.message);
-          res.redirect("/signup");
-
-        case 15:
+        case 8:
         case "end":
           return _context.stop();
       }
     }
-  }, null, null, [[0, 11]]);
-}));
+  });
+})); // Login route
+
 router.get("/login", function (req, res) {
   res.render("users/login");
 });
-router.post("/login", passport.Authenticator("local", {
+router.post("/login", passport.authenticate("local", {
   failureRedirect: "/login",
   failureFlash: true
 }), function _callee2(req, res) {
@@ -61,9 +54,10 @@ router.post("/login", passport.Authenticator("local", {
     while (1) {
       switch (_context2.prev = _context2.next) {
         case 0:
-          res.send("welcome to roomify you are logged in");
+          req.flash("success", "welcome to with your account");
+          res.redirect("/listings");
 
-        case 1:
+        case 2:
         case "end":
           return _context2.stop();
       }
--- routes/user.js
+++ routes/user.js
@@ -1,46 +1,48 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
-const wrapAsyc = require("../utils/wrapAsyc");
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsyc.js");
 const passport = require("passport");
 
 
-
+// Signup route
 router.get("/signup", (req, res) => {
     res.render("users/signup");
-});  
+});
 
 
+router.post(
+    "/signup",
+    wrapAsync(async (req, res) => {
+        let { username, email, password } = req.body;
+        const newUser = new User({ email, username });
+        const registeredUser = await User.register(newUser, password);
+        console.log(registeredUser);
+        req.flash("success", "User was registered successfully!");
+        res.redirect("/listings");
+    })
+);
 
-router.post("/signup",wrapAsyc( async(req, res)=>{
-    try{
-        let{username, email, password} = req.body;
-    const newuser = new User({email, username});
-    const registerUser = await User.register(newuser, password);
-    console.log(registerUser);
-    req.flash("success","user was registered");
-    res.redirect("/listings");
-    }
-    catch(e){
-        req.flash("error", e.message);
-        res.redirect("/signup");
-    }
-    
-}));
 
-router.get("/login", (req, res)=>{
+// Login route
+router.get("/login", (req, res) => {
     res.render("users/login");
 });
 
-router.post("/login" ,passport.Authenticator("local", 
-    {failureRedirect : "/login",
-        failureFlash : true
-    }    
-), async (req, res)=>{
-    res.send("welcome to roomify you are logged in");
-});
 
-
+router.post(
+    "/login",
+    passport.authenticate("local", {
+        failureRedirect: "/login",
+        failureFlash: true,
+    }),
+    async (req, res) => {
+        req.flash("success", "welcome to with your account");
+        res.redirect("/listings");
+    }
+);
 
 
 module.exports = router;
+
+
