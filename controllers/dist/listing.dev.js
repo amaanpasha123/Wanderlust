93b778fb131f12e879ffc5e4e6982e6a1f615e08
Amaan Pasha <Amaanpasha23@gmail.com>
2025-02-06 00:35:53 +0530

    Controller file is added here

---
+++ controllers/dist/listing.dev.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var Listing = require("../models/listing");
+
+module.exports.index = function _callee(req, res) {
+  var allListings;
+  return regeneratorRuntime.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return regeneratorRuntime.awrap(Listing.find({}));
+
+        case 2:
+          allListings = _context.sent;
+          res.render("listings/index", {
+            allListings: allListings
+          });
+
+        case 4:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+};
