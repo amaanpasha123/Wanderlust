c7fed184ce2b49a37f44e898a8b7faddf997e42f
Amaan Pasha <Amaanpasha23@gmail.com>
2025-02-15 19:54:53 +0530

    new one

--- dist/cloudconfig.dev.js
+++ dist/cloudconfig.dev.js
@@ -4,3 +4,9 @@ var cloudinary = require('cloudinary').v2;
 
 var _require = require('multer-storage-cloudinary'),
     CloudinaryStorage = _require.CloudinaryStorage;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
+});
